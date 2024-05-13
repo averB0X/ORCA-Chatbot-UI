4bbf3c890572cb30dd4749fc4bf1c74a5698bcc3
aver.BOX <lryuzakigallego@gmail.com>
2024-05-13 19:12:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,23 +6,20 @@ from chatbotv1 import getResponse
 
 app = Flask(__name__)
 
+dictionary = open("intents/intents.json").read()
+intents = json.loads(dictionary)
+
 @app.route("/")
 def index_get():
-    return render_template('index.html')
-
+    return render_template("index.html")
 
 @app.post("/predict")
 def predict():
-    dictionary = open('intents/intents.json').read()
-    intents = json.loads(dictionary)
-    msg = input("User: ")
-    if msg == "debugquit":
-        exit()
-    else:
-        ints = predictClass(msg)
-        res = getResponse(ints, intents)
-        return jsonify(res)
+    msg = request.get_json().get("msg")
+    ints = predictClass(msg)
+    res = getResponse(ints, intents)
+    return jsonify(res)
         
         
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="127.0.0.1", port="8080", debug=True)
